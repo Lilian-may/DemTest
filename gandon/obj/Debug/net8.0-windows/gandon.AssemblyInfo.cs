c6643d9059521d36b4c8e1365ae9dea907c87d23
Lilian-may <artur2005kuzmin@gmail.com>
2026-05-30 20:38:51 +0400

    feat: delete artifacts

--- gandon/obj/Debug/net8.0-windows/gandon.AssemblyInfo.cs
+++ gandon/obj/Debug/net8.0-windows/gandon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gandon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69ad0ccba7e1d95b5113cd4bfc23f124b5f377b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("gandon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gandon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
